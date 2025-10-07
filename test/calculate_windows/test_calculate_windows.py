e9c97c0831d60c50c1a0c01b5872756ef48a3212
Keith Oshima <koshima789@gmail.com>
2025-10-06 23:44:39 -0400

    Remove unused files. Fix test.

--- test/calculate_windows/test_calculate_windows.py
+++ test/calculate_windows/test_calculate_windows.py
@@ -8,8 +8,6 @@ from test.helpers.integration import run_integration_test
         "methylation_tsv",
         "window_size",
         "exp_bed",
-        # The output produced by the original script.
-        "exp_og_bed",
     ],
     [
         (
@@ -17,7 +15,6 @@ from test.helpers.integration import run_integration_test
             "test/calculate_windows/input/CHM13_methyl.bed",
             5000,
             "test/calculate_windows/expected/expected_bin_freq.bed",
-            "test/calculate_windows/expected/original_bin_freq.bed",
         )
     ],
 )
@@ -26,16 +23,7 @@ def test_calculate_windows(
     methylation_tsv: str,
     window_size: int,
     exp_bed: str,
-    exp_og_bed: str,
 ):
-    with open(exp_bed) as fh, open(exp_og_bed) as ofh:
-        exp_regions = sorted(
-            enumerate(line.strip().split("\t") for line in fh.readlines()),
-            key=lambda x: x[1],
-        )
-        exp_og_regions = sorted(line.strip().split("\t") for line in ofh.readlines())
-
-    # The command output equals the expected output.
     run_integration_test(
         "python",
         "workflow/scripts/calculate_windows.py",
@@ -47,12 +35,3 @@ def test_calculate_windows(
         str(window_size),
         expected_output=exp_bed,
     )
-
-    # The expected output equals the original output.
-    for (i, exp_region), exp_og_region in zip(exp_regions, exp_og_regions):
-        *coords, freq, _ = exp_region
-        *ecoords, efreq, ei = exp_og_region
-        freq, efreq = float(freq), float(efreq)
-        assert str(i) == ei and coords == ecoords
-        # Allow some approximation for floating point.
-        assert freq == pytest.approx(efreq, 0.5)
